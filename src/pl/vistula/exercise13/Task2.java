f8def140e8ae1f2160c24491367e4769f0834978
Tigistgudeta2009 <tigistgudeta2021@gamil.com>
2022-05-31 14:09:16 -0700

    Initial commit

---
+++ src/pl/vistula/exercise13/Task2.java
@@ -0,0 +1,43 @@
+package pl.vistula.exercise13;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Task2 {
+    public static void main(String[] args) throws IOException {
+            List<String> employees = new ArrayList<>();
+            try (BufferedReader br = new BufferedReader(new FileReader("Names.txt"))) {
+
+                String line;
+                while ((line = br.readLine()) != null) {
+                    employees.add(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //printing the size of collections
+
+            System.out.println("Size of the collection is : "+employees.size());
+
+            //printing the content using :
+            //  For Each loop :
+            System.out.println("--------------------------------------------- ");
+            for (String employee:employees
+            ) {
+                System.out.println(employee);
+            }
+            //Hash Set to prevent the repetition
+            HashSet<String> set = new HashSet<String>(employees);
+            // new list with no repitions
+            List<String> newls = new ArrayList<String>(set);
+            System.out.println("--------------------------------------------- ");
+            System.out.println("List after removing duplicate elements:");
+            for (Object ob: newls)
+                System.out.println(ob);
+
+        }
+}
